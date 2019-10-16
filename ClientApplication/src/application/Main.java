6643bd7c3185a058bbb48066f400086e8c54387e
Bernardo <lbct95@gmail.com>
2019-10-16 09:25:49 -0400

    Tray Icon implementation

--- ClientApplication/src/application/Main.java
+++ ClientApplication/src/application/Main.java
@@ -4,6 +4,15 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import view.LoginPageView;
 import model.LoginPageModel;
+
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import controller.LoginPageController;
 import javafx.scene.Scene;
 
@@ -11,6 +20,10 @@ import javafx.scene.Scene;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Image image = toolkit.getImage("resources/icons/icon.png");
+		addTrayIcon(image);
 		
 		LoginPageModel loginModel = new LoginPageModel();
 		LoginPageController loginController = new LoginPageController(loginModel);
@@ -28,5 +41,26 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private void addTrayIcon(Image image) {
+		if (SystemTray.isSupported()) {
+		   TrayIcon trayIcon = new TrayIcon(image);
+		   trayIcon.setImageAutoSize(true);
+
+		   trayIcon.addMouseListener(new MouseAdapter() {
+		      @Override
+		      public void mouseClicked(MouseEvent e) {
+		         System.out.println("Clicked");
+		      }
+		   });
+		   try {
+		      SystemTray.getSystemTray().add(trayIcon);
+		   } catch (AWTException ex) {
+		      System.err.println("Error while creating tray icon.");
+		   }
+		} else {
+		   System.err.println("Tray icons are not supported on this System.");
+		}
 	}
 }
